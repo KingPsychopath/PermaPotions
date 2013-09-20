e699ece0f021069b3716503b8889b57e0ee236d5
Ryan Marple <ryan_marple@hotmail.com>
2013-09-20 15:56:40 +1000

    Version 0.2.1 release
    No known bugs

---
+++ src/me/Ryan6338/PermaPotion/PlayerListener.java
@@ -0,0 +1,34 @@
+package me.Ryan6338.PermaPotion;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PotionSplashEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerListener implements Listener {
+
+	private Main main;
+	
+	//Constructor to get instance of main
+	public PlayerListener(Main instance) {
+		main = instance;
+	}
+	
+	@EventHandler (priority = EventPriority.HIGH)
+	public void onPlayerJoinEvent(PlayerJoinEvent e) {
+		main.playerJoined(e.getPlayer());
+	}
+	
+	@EventHandler
+	public void onPotionSplash(PotionSplashEvent e) {
+		for (Entity ent : e.getAffectedEntities()) {
+			if (ent instanceof Player) {
+				main.playerHitByPotion((Player) ent, e.getPotion());
+			}
+		}
+	}
+	
+}
